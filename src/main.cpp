0c09988adf76dc151cb3573cc74d4074e9bd533b
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-03-09 21:37:13 +0500

    misc

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,37 @@ double lastUpdate = 0;
 bool EventTriggered(double interval);
 
 int main() {
-    
+
     Color bg = {1,1,20,255};
     double blockSpeed = 0.5f;
     GameHandler gameHandler;
-    
+
     InitWindow(600, 650, "Tetris");
 
     Font TetrisFont = LoadFontEx("assets/fonts/gameFont.ttf", 32, 0, 250);
-    
+
     SetTargetFPS(60);
-    
+
     float deltaTime = 0.0f;
 
     while (!WindowShouldClose()) {
-    
-        float startTime = GetTime();  
-        
+
+        float startTime = GetTime();
+
         gameHandler.InputHandler(deltaTime);
         UpdateMusicStream(gameHandler.bgMusic);
 
         BeginDrawing();
         ClearBackground(bg);
-        
+
         DrawTextEx(TetrisFont, "Score", {400, 50}, 36, 4, WHITE);
-        
+
         char scoreText[10];
         sprintf(scoreText, "%d", gameHandler.score);
         Vector2 TextSize = MeasureTextEx(TetrisFont, scoreText, 36, 2);
         DrawRectangleRounded({370, 100, 170, 60}, 0.3, 6, DARKGRAY);
         DrawTextEx(TetrisFont, scoreText, {385 + (140 - TextSize.x)/2, 110}, 36, 2, WHITE);
- 
+
         DrawTextEx(TetrisFont, "Next Block", {370, 200}, 32, 4, WHITE);
         DrawRectangleRounded({370, 250, 170, 170}, 0.3, 6, DARKGRAY);
         if(gameHandler.isGameOver()){
@@ -49,7 +49,7 @@ int main() {
             gameHandler.moveDown();
 
         gameHandler.Draw();
-        
+
         EndDrawing();
         deltaTime = GetTime() - startTime;
     }
